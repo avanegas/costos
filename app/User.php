731121708926ae8e101bf0d488f95503b2cf02e8
vagrant <vagrant@homestead>
2018-12-16 21:28:48 +0000

    users

--- app/User.php
+++ app/User.php
@@ -20,20 +20,10 @@ class User extends Authenticatable
 {
     use Notifiable, DatePresenter, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'api_token'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token', 'api_token'
     ];
@@ -42,12 +32,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
-    
-    public function setPasswordAttribute($password)
-    {   
-        $this->attributes['password'] = bcrypt($password);
-    }
-    
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
